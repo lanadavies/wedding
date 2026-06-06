624681e7ba14f1ec811ffe1f81316ddada5c9e8d
Allana Davies <s3720730@student.rmit.edu.au>
2026-06-07 00:00:23 +1000

    Nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Monogram from "./Monogram";
 export default function NavBar() {
     return (
         <nav className="w-full paper-background h-16 flex justify-center items-center">
-            <Monogram size="50px" />
+            <Monogram size="100px" oval={false} />
         </nav>
     );
 }
