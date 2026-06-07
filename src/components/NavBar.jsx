93314e8bf7ca35f58652fc4c098ec5fa4ab27238
Allana Davies <s3720730@student.rmit.edu.au>
2026-06-07 12:34:00 +1000

    Added decor to banner

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,28 @@
 import Monogram from "./Monogram";
+import Decor from "../assets/vintage_frame_18_half.png";
+
 
 export default function NavBar() {
     return (
         <nav className="w-full paper-background h-16 flex justify-center items-center">
+            <div className="bg-amber-100 w-[2rem] h-[1rem] -mr-2"
+                style={{
+                    WebkitMaskImage: `url(${Decor})`,
+                    maskImage: `url(${Decor})`,
+                    maskSize: 'contain',
+                    maskRepeat: 'no-repeat'
+                }}
+            />
             <Monogram size="100px" oval={false} />
+            <div className="bg-amber-100 w-[2rem] h-[1rem] -ml-2"
+                style={{
+                    WebkitMaskImage: `url(${Decor})`,
+                    maskImage: `url(${Decor})`,
+                    maskSize: 'contain',
+                    maskRepeat: 'no-repeat',
+                    transform: 'scaleX(-1)'
+                }}
+            />
         </nav>
     );
 }
