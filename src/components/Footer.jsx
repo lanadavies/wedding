89d2985598b6d882896285051ce0387d9553ee69
Allana Davies <s3720730@student.rmit.edu.au>
2026-06-08 21:46:29 +1000

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import Monogram from "./Monogram";
+import Frame from "../assets/vintage_frame_set_2_02.png";
 
 export default function Footer() {
     return (
-        <footer className="w-full my-6 flex justify-center items-center">
-            <Monogram size="6rem" />
+        <footer className="w-full my-6 flex justify-center items-center relative">
+            <div
+                className="bg-amber-100 w-[10rem] h-[7rem] absolute -top-6"
+                style={{
+                    WebkitMaskImage: `url(${Frame})`,
+                    maskImage: `url(${Frame})`,
+                    maskSize: 'contain',
+                    maskRepeat: 'no-repeat'
+                }}
+            />
+            <div>
+                <Monogram size="6rem" />
+            </div>
         </footer>
     );
 }
