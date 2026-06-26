45c38b36d70fd107138ab18f7e1a6d8dcb0b585f
lanadavies <129735036+lanadavies@users.noreply.github.com>
2026-06-26 10:54:59 +0000

    Banner photo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
     return (
         <div
             style={{
-                backgroundImage: `url(https://static.showit.co/2400/HQomN8EVqQuR7DyRn2_FKA/shared/storybook-couple-garden-walk-shauna-maxwell-photography.jpg)`
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("DSC_1378.JPG")`
             }}
             className="p-8 lg:mx-[3rem] relative py-[15rem] w-full lg:w-[calc(100%-3rem)] flex flex-col items-center bg-cover bg-center"
         >
