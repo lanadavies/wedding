8bc650b267304634a848ffbcef52209e555bebd0
lanadavies <129735036+lanadavies@users.noreply.github.com>
2026-06-26 15:03:13 +0000

    Background continuity

--- src/components/RSVPSection.jsx
+++ src/components/RSVPSection.jsx
@@ -9,7 +9,7 @@ import Flower from "../assets/flower.png";
 export default function RSVPSection() {
     return (
         <div
-            className='h-[25rem] w-full grid place-items-center relative'
+            className='h-[25rem] w-full grid place-items-center relative paper-background'
         >
 			<img src={Flower} className="absolute w-[70px] lg:w-[6rem] left-0 lg:left-110 top-32 object-contain z-25 pointer-events-none -rotate-15" />
             <img src={Tape} className="absolute w-[60px] lg:w-[6rem] left-2 lg:left-110 top-55 object-contain z-50 pointer-events-none -rotate-10" />
