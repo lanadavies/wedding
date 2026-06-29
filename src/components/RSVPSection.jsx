35afd4d46ac20cd0be0bad54e82de788ae86d486
lanadavies <129735036+lanadavies@users.noreply.github.com>
2026-06-29 09:49:41 +0000

    Flower fix

--- src/components/RSVPSection.jsx
+++ src/components/RSVPSection.jsx
@@ -19,8 +19,8 @@ export default function RSVPSection() {
         >
             <Link to="/rsvp" aria-label="RSVP">
                 <motion.div className="text-amber-100 h-[11rem] w-[21rem] relative" whileHover={{ scale: 1.05 }}>
-                    <img src={Flower} className="absolute w-[70px] lg:w-[6rem] -left-10 top-10 object-contain z-25 pointer-events-none -rotate-15" />
-                    <img src={Tape} className="absolute w-[60px] lg:w-[6rem] -left-10 top-30 object-contain z-50 pointer-events-none -rotate-10" />
+                    <img src={Flower} className="absolute w-[70px] -left-10 top-10 object-contain z-25 pointer-events-none -rotate-15" />
+                    <img src={Tape} className="absolute w-[60px] -left-10 top-30 object-contain z-50 pointer-events-none -rotate-10" />
                     <img
                         src={EnvelopeTopFlap}
                         alt="Envelope back"
